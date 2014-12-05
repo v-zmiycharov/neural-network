995b8150e710460b98c8a0859bdf61ed30776a3c
v-zmiycharov <valentin.zmiycharov@gmail.com>
2014-12-05 17:00:19 +0200

    Implement calculation for mistakes without sum

--- src/NeuralNetwork/InputItem.cs
+++ src/NeuralNetwork/InputItem.cs
@@ -10,28 +10,44 @@ namespace NeuralNetwork
         public double[] Attributes { get; set; }
         public int Value { get; set; }
 
-        public double[] MiddleLayer { get; set; }
-        public double OutputLayer { get; set; }
+        public Neuron[] MiddleLayer { get; set; }
+        public Neuron OutputLayer { get; set; }
 
         public InputItem()
         {
             this.Attributes = new double[Constants.ATTRIBUTES_COUNT];
 
-            this.MiddleLayer = new double[Constants.MIDDLE_LAYER_NEURONS_COUNT];
+            this.MiddleLayer = new Neuron[Constants.MIDDLE_LAYER_NEURONS_COUNT];
         }
 
         public void CalculateLayerValues()
         {
             for (int i = 0; i < this.MiddleLayer.Length; i++)
             {
+                this.MiddleLayer[i] = new Neuron();
+
                 double sum = Enumerable.Range(0, Constants.ATTRIBUTES_COUNT)
                     .Sum(start => Roads.GetInitial(start, i) * this.Attributes[start]);
-                this.MiddleLayer[i] = (double)(1/(1+Math.Exp(-sum)));
+                this.MiddleLayer[i].Value = (double)(1 / (1 + Math.Exp(-sum)));
             }
 
+            this.OutputLayer = new Neuron();
+
             double middleLayerSum = Enumerable.Range(0, Constants.MIDDLE_LAYER_NEURONS_COUNT)
-                    .Sum(start => Roads.GetLast(start) * this.MiddleLayer[start]);
-            this.OutputLayer = (double)(1 / (1 + Math.Exp(-middleLayerSum)));
+                    .Sum(start => Roads.GetLast(start) * this.MiddleLayer[start].Value);
+            this.OutputLayer.Value = (double)(1 / (1 + Math.Exp(-middleLayerSum)));
+        }
+
+        public void CalculateLayerMistakes()
+        {
+            for (int i = 0; i < this.MiddleLayer.Length; i++)
+            {
+                double newValue = this.MiddleLayer[i].Value;
+                this.MiddleLayer[i].Mistake = newValue * (1 - newValue) * 1;
+            }
+
+            double calculatedValue = this.OutputLayer.Value;
+            this.OutputLayer.Mistake = calculatedValue * (1 - calculatedValue) * (this.Value - calculatedValue);
         }
     }
 }
---
+++ src/NeuralNetwork/Neuron.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NeuralNetwork
+{
+    public class Neuron
+    {
+        public double Value { get; set; }
+        public double Mistake { get; set; }
+    }
+}
